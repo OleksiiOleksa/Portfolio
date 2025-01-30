2ee2726a03fe2a37643db4e0f61a3e69b99e9218
OleksiiOleksa <benedysiuk.oleksii@gmail.com>
2025-01-30 20:09:02 +0100

    settings_wsgi

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+port = os.environ.get('PORT', 8000)
+
+port = os.environ.get('PORT', 8080)
