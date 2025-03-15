f9b57d4f90b4b8c24ff881b0ee29d7dfcebe5d5e
OleksiiOleksa <benedysiuk.oleksii@gmail.com>
2025-03-15 14:21:32 +0100

    settings13

--- mysite/taskmanager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'priority', 'is_completed', 'created_at')
-    list_filter = ('priority', 'is_completed', 'created_at')
-    search_fields = ('title', 'description')
--- mysite/taskmanager/forms.py
+++
@@ -1,9 +0,0 @@
-# taskmanager/forms.py
-
-from django import forms
-from .models import Task
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'priority']
